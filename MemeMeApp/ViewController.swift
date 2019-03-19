18c559ef62e7d849eac99f3e48c24aa651a78309
AlekseyK <aleksey.kabishau@gmail.com>
2019-03-18 23:28:45 -0400

    initial clean up

--- MemeMeApp/ViewController.swift
+++ MemeMeApp/ViewController.swift
@@ -1,20 +1,10 @@
-//
-//  ViewController.swift
-//  MemeMeApp
-//
-//  Created by Aleksey on 0318..19.
-//  Copyright © 2019 Aleksey. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
 }
 
