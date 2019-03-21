7793a367ea9ad01d6bf088f55c416e53adb8b761
AlekseyK <aleksey.kabishau@gmail.com>
2019-03-20 23:36:16 -0400

    work in progress

--- MemeMeApp/ViewController.swift
+++ MemeMeApp/ViewController.swift
@@ -2,9 +2,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    // is there any other convenient way to customize the text in text field?
+    // need to play with this in order to make it perfect
+    let textAttributes: [NSAttributedString.Key: Any] = [
+        .strokeColor: UIColor.black,
+        .strokeWidth: 3.0,
+        .foregroundColor: UIColor.white,
+        // TODO: find the "Impact" Font - requirements
+        .font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        
+        
+        // TODO: make it all CAPS
+        topTextField.text = "TOP"
+        // FIXME: Alignment stopped working
+        topTextField.defaultTextAttributes = textAttributes
+        topTextField.textAlignment = .center
+        
+        // TODO: customize this text field
+        bottomTextField.text = "BOTTOM"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .camera
+        present(pickerController, animated: true, completion: nil)
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+// navigation delegate doesn't have any required methods
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage {
+            imageView.image = image
+        }
+        
+        dismiss(animated: true, completion: { print("picked") })
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        dismiss(animated: true) {
+            print("canceled")
+        }
+    }
+}
