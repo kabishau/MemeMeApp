61364163cfd5e54f71625599fb77ce8e1ebb2385
AlekseyK <aleksey.kabishau@gmail.com>
2019-04-14 00:16:22 -0400

    temp array to see the result

--- MemeMeApp/AppDelegate.swift
+++ MemeMeApp/AppDelegate.swift
@@ -5,8 +5,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    var memes = [Meme]()
-
+    //var memes = [Meme]()
+    var memes: [Meme] = [Meme(topText: "TopText", bottomText: "BottomText", originalImage: UIImage(named: "nssl0045")!, memedImage: UIImage(named: "nssl0045")!)]
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         return true
