cc6d5043043120d6d49d19d5d1a45dda6ee5903c
AlekseyK <aleksey.kabishau@gmail.com>
2019-04-15 23:46:21 -0400

    basic collection view and flow layout

--- MemeMeApp/Controller/MemeCollectionViewController.swift
+++ MemeMeApp/Controller/MemeCollectionViewController.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class MemeCollectionViewController: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    //TODO: is this a best practice?
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
     var memes: [Meme] {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         return appDelegate.memes
@@ -9,6 +13,52 @@ class MemeCollectionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let space: CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3
+        flowLayout.minimumLineSpacing = space
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+        
+        title = "Sent Memes"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+    }
+    
+    @objc func addButtonTapped() {
+        guard let memeEditorViewController = storyboard?.instantiateViewController(withIdentifier: "MemeEditorViewController") as? MemeEditorViewController else {
+            return
+        }
+        navigationController?.pushViewController(memeEditorViewController, animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //TODO: not efficient to reload
+        collectionView.reloadData()
+    }
+    
+}
 
+extension MemeCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        cell.imageView.image = memes[indexPath.item].originalImage
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let detailViewController = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController else { return }
+        detailViewController.memedImage = memes[indexPath.item].memedImage
+        
+        navigationController?.pushViewController(detailViewController, animated: true)
     }
 }
