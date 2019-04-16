6d4769c0469d658dc6c506b5e4a5dd91c2c4c00b
AlekseyK <aleksey.kabishau@gmail.com>
2019-04-15 23:45:50 -0400

    temp memes

--- MemeMeApp/AppDelegate.swift
+++ MemeMeApp/AppDelegate.swift
@@ -6,7 +6,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     //var memes = [Meme]()
-    var memes: [Meme] = [Meme(topText: "TopText", bottomText: "BottomText", originalImage: UIImage(named: "nssl0045")!, memedImage: UIImage(named: "nssl0045")!)]
+    var memes: [Meme] = [
+        Meme(topText: "", bottomText: "", originalImage: UIImage(named: "meme1")!, memedImage: UIImage(named: "meme1")!),
+        Meme(topText: "", bottomText: "", originalImage: UIImage(named: "meme2")!, memedImage: UIImage(named: "meme2")!),
+        Meme(topText: "", bottomText: "", originalImage: UIImage(named: "meme3")!, memedImage: UIImage(named: "meme3")!),
+        Meme(topText: "", bottomText: "", originalImage: UIImage(named: "meme4")!, memedImage: UIImage(named: "meme4")!),
+        Meme(topText: "", bottomText: "", originalImage: UIImage(named: "meme5")!, memedImage: UIImage(named: "meme5")!)]
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
