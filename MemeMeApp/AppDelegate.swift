f222130d3cf4dd5d0fc8f0e8160ba7590e6a7856
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-04-26 16:29:58 -0400

    updated text for labels for preexisting memes

--- MemeMeApp/AppDelegate.swift
+++ MemeMeApp/AppDelegate.swift
@@ -7,12 +7,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     //var memes = [Meme]()
     var memes: [Meme] = [
-        Meme(topText: "Top1", bottomText: "Bottom1", originalImage: UIImage(named: "But-Thats-None-Of-My-Business")!, memedImage: UIImage(named: "Meme-But-Thats-None-Of-My-Business")!),
-        Meme(topText: "Top2", bottomText: "Bottom2", originalImage: UIImage(named: "Chuck-Norris-Guns")!, memedImage: UIImage(named: "Meme-Chuck-Norris-Guns")!),
-        Meme(topText: "Top3", bottomText: "Bottom3", originalImage: UIImage(named: "Face-You-Make-Robert-Downey-Jr")!, memedImage: UIImage(named: "Meme-Face-You-Make-Robert-Downey-Jr")!),
-        Meme(topText: "Top4", bottomText: "Bottom4", originalImage: UIImage(named: "Leonardo-Dicaprio-Cheers")!, memedImage: UIImage(named: "Meme-Leonardo-Dicaprio-Cheers")!),
-        Meme(topText: "Top5", bottomText: "Bottom5", originalImage: UIImage(named: "Skeptical-Baby")!, memedImage: UIImage(named: "Meme-Skeptical-Baby")!),
-        Meme(topText: "Top5", bottomText: "Bottom5", originalImage: UIImage(named: "Waiting-Skeleton")!, memedImage: UIImage(named: "Meme-Waiting-Skeleton")!)        
+        Meme(topText: "THAT KIDS GETTING HARRASED", bottomText: "BUT THAT'S NONE OF MY BUSINESS", originalImage: UIImage(named: "But-Thats-None-Of-My-Business")!, memedImage: UIImage(named: "Meme-But-Thats-None-Of-My-Business")!),
+        Meme(topText: "I ONCE SHOT 11 OUT OF 10", bottomText: "TARGETS WITH 9 BULLETS", originalImage: UIImage(named: "Chuck-Norris-Guns")!, memedImage: UIImage(named: "Meme-Chuck-Norris-Guns")!),
+        Meme(topText: "WHEN YOU SEE A 30 SECOND YOUTUBE AD", bottomText: "WITHOUT SKIP BUTTON", originalImage: UIImage(named: "Face-You-Make-Robert-Downey-Jr")!, memedImage: UIImage(named: "Meme-Face-You-Make-Robert-Downey-Jr")!),
+        Meme(topText: "HEY YOU", bottomText: "WANNA GO TO MCDONALDS?", originalImage: UIImage(named: "Leonardo-Dicaprio-Cheers")!, memedImage: UIImage(named: "Meme-Leonardo-Dicaprio-Cheers")!),
+        Meme(topText: "OHHH", bottomText: "SO YOU THINK I DON'T SEE YOU EH?", originalImage: UIImage(named: "Skeptical-Baby")!, memedImage: UIImage(named: "Meme-Skeptical-Baby")!),
+        Meme(topText: "WHEN YOUR MOM", bottomText: "FORGOT TO PICK YOU UP", originalImage: UIImage(named: "Waiting-Skeleton")!, memedImage: UIImage(named: "Meme-Waiting-Skeleton")!)        
     ]
     
     
